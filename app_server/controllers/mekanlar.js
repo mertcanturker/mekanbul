e2c02197743a89754a45adb7003a08912c6554b2
lennie7 <mertcan_turker32@hotmail.com>
2022-10-20 17:05:32 +0300

    last_commit

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -2,71 +2,12 @@ var express = require('express');
 var router = express.Router();
 
  const anaSayfa=function(req, res, next) {
-    res.render('anasayfa', { 
-      "baslik": "Anasayfa",
-      "sayfaBaslik": {
-        "siteAd": "MekanBul",
-        "slogan": "Civardaki Mekanları Keşfet!"
-      },
-      "mekanlar": [
-        {
-          "ad": "Starbucks",
-          "adres": "Centrum Garden AVM",
-          "puan": "4",
-          "imkanlar": ["Dünya Kahveleri","Kekler","Pastalar"],
-          "mesafe": "10km"
-        }
-        ,
-        {
-          "ad": "Gloria Jean's Coffees",
-          "adres": "SDÜ Doğu Kampüsü",
-          "puan": "3",
-          "imkanlar": ["Kahve","Çay","Pasta"],
-          "mesafe": "5km"
-        }
-      ]
-    }
-    );
-  }
+    res.render('anasayfa', { title: 'Anasayfa' });
+  };
 
   const mekanBilgisi=function(req, res, next) {
-    res.render('mekanbilgisi',
-      {
-        "baslik": "Mekan Bilgisi",
-        "mekanBaslik": "Starbucks",
-        "mekanDetay": {
-          "ad": "Starbucks",
-          "adres": "Centrum Garden AVM",
-          "puan": "4",
-          "imkanlar": ["Dünya Kahveleri","Kekler","Pastalar"],
-          "koordinatlar": {
-            "enlem": "37.7",
-            "boylam":"30.5"
-          },
-          "saatler":[{
-            "gunler":"Pazartesi-Cuma",
-            "acilis":"9:00",
-            "kapanis":"23:00",
-            "kapali": false
-        },
-        {
-            "gunler":"Cumartesi-Pazar",
-            "acilis":"10:00",
-            "kapanis":"22:00",
-            "kapali": false
-        }
-    ],
-          "yorumlar": [
-            {
-              "yorumYapan":"Mertcan Türker",
-              "puan":"4",
-              "tarih":"20 Ekim 2022",
-              "yorumMetni":"Kahveler iyi."
-            },
-          ]
-        }
-      });
-  }
+    res.render('mekanbilgisi', { title: 'Mekan Bilgisi' });
+  };
 
   const YorumEkle=function(req, res, next) {
     res.render('yorumekle', { title: 'Yorum Ekle' });
