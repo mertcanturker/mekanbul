99722d14f5e844d4833e4b2c142db3f9be2770a7
lennie7 <mertcan_turker32@hotmail.com>
2022-11-10 21:18:25 +0300

    odev4mongodb

---
+++ app_server/models/db.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+// var dbURI='mongodb://localhost/mekanbul';
+var dbURI="mongodb+srv://mertcan:1234@mekanbul.kaejpdz.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(dbURI);
+function kapat(msg,callback){
+    mongoose.connection.close(function(){
+        console.log(msg);
+        callback();
+    });
+
+}
+process.on("SIGINT",function(){
+    kapat("Uygulama kapatıldı", function(){
+        process.exit(0);
+    });
+});
+mongoose.connection.on("connected",function(){
+    console.log(dbURI+" adresindeki veritabanına bağlandı!\n");
+});
+mongoose.connection.on("disconnected",function(){
+    console.log(dbURI+" Bağlantı koptu");
+});
+mongoose.connection.on("error",function(){
+    console.log(dbURI+" Bağlantı hatası");
+});
+
+require("./mekansema");
+
