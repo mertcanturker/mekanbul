461a050822b57c1424a0692e7f641c1cb86f1b6c
lennie7 <mertcan_turker32@hotmail.com>
2022-12-01 19:40:09 +0300

    odev7

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,10 +2,9 @@ var express = require('express');
 var router = express.Router();
 var ctrlMekanlar = require('../controllers/mekanlar');
 var ctrlDigerleri = require('../controllers/digerleri');
-/* GET home page. */
-router.get('/',ctrlMekanlar.anaSayfa);
-router.get('/mekan/:mekanid',ctrlMekanlar.mekanBilgisi);
-router.get('/mekan/yorum/yeni',ctrlMekanlar.YorumEkle);
-router.get('/hakkinda',ctrlDigerleri.hakkinda);
 
+router.get('/',ctrlMekanlar.anaSayfa);
+router.get('/mekan/:mekanid', ctrlMekanlar.mekanBilgisi);
+router.get('/mekan/yorum/yeni',ctrlMekanlar.yorumEkle);
+router.get('/hakkinda',ctrlDigerleri.hakkinda);
 module.exports = router;
